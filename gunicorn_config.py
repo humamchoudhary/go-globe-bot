1e228fe3716a9af108c3390c796a3d00566c7daa
Muhammad Humam <humamchoudhary@gmail.com>
2025-04-04 19:46:23 +0500

    wip

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,15 @@
+# # import multiprocessing
+#
+# bind = "0.0.0.0:5000"
+#
+# # Use eventlet or gevent for WebSockets
+# worker_class = "eventlet"  # OR "gevent"
+#
+# workers = 1  # Recommended worker count
+# accesslog = "logs/access.log"
+# errorlog = "logs/error.log"
+# loglevel = "info"
+#
 # import multiprocessing
 
 bind = "0.0.0.0:5000"
@@ -6,7 +18,9 @@ bind = "0.0.0.0:5000"
 worker_class = "eventlet"  # OR "gevent"
 
 workers = 1  # Recommended worker count
-accesslog = "logs/access.log"
-errorlog = "logs/error.log"
+
+logfile = "logs/gunicorn.log"
+accesslog = logfile
+errorlog = logfile
 loglevel = "info"
 
