25d735ec8bf26aeb694784a23ff8a80ca1b32682
Muhammad Humam <humamchoudhary@gmail.com>
2025-05-10 00:12:34 +0500

    wip

--- templates/js/init_chat.js
+++ templates/js/init_chat.js
@@ -26,7 +26,7 @@
     right: 20px;
     width: 90vw;
     max-width: 400px;
-    height: 70vh;
+ height: 600px;
     background-color: white;
     border-radius: 10px;
     box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
