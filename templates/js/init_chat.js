a5b2af5cf3648a76b9379c2350abb0bc6fc6ca39
Muhammad Humam <humamchoudhary@gmail.com>
2025-05-22 00:49:09 +0500

    wip

--- templates/js/init_chat.js
+++ templates/js/init_chat.js
@@ -166,10 +166,9 @@
     setTimeout(() => {
       const chatContainer = document.getElementById("chat-container");
       if (chatContainer) {
-        chatContainer.style.display = "flex"; // or "block" depending on your layout needs
-
         const audio = new Audio("{{backend_url}}/static/sounds/pop-up.wav");
         audio.play();
+        chatContainer.style.display = "flex"; // or "block" depending on your layout needs
       }
     }, 4000); // 4000ms = 4 seconds
   });
