a20007c9548e32c8556c25fcf9047f43bee7e2fb
Muhammad Humam <humamchoudhary@gmail.com>
2025-05-24 00:42:19 +0500

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html"],
+  content: ["./templates/*.html"],
 
   corePlugins: {
     preflight: false, // This disables Tailwind's global base/reset styles
