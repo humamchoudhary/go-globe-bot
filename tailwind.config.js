518df9d65f39d44298b7136a96cf31ee40f65ec2
Muhammad Humam <humamchoudhary@gmail.com>
2025-05-24 00:04:58 +0500

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+
+  corePlugins: {
+    preflight: false, // This disables Tailwind's global base/reset styles
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
