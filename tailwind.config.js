04de243b96951a3ad0eb531a8f62327d940f219e
Muhammad Humam <humamchoudhary@gmail.com>
2025-05-24 00:30:34 +0500

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html", // adjust path to your HTML/Jinja files
-    "./static/**/*.js",
-    "./*.html",
+  content: ["./**/*.html"],
+  safelist: [
+    "placeholder-[var(--goglobe-body-color)]",
+    "bg-[var(--goglobe-input-color)]",
+    "focus:bg-[var(--goglobe-site-bg-color)]",
+    // add other dynamic classes here
   ],
-
   corePlugins: {
     preflight: false, // This disables Tailwind's global base/reset styles
   },
