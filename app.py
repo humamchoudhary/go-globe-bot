04e812ada5446cde38dacd985f4b80738b3048fa
humamchoudhary <humamchoudhary@gmail.com>
2025-07-22 18:02:54 +0500

    ui change

--- app.py
+++ app.py
@@ -55,9 +55,8 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     CORS(app, origins=["*"],
          supports_credentials=True,
-         allow_headers=["*"],
-         # expose_headers=["Content-Disposition"],
-         methods=['*']
+         allow_headers=['Content-Type'], methods=['GET', 'POST', 'OPTIONS'],
+         expose_headers=["Content-Disposition"],
          )
     app.config.from_object(config_class)
 
@@ -627,6 +626,7 @@ def create_app(config_class=Config):
         arguments = rule.arguments if rule.arguments is not None else ()
         return len(defaults) >= len(arguments)
     import markdown
+
     @app.template_filter('markdown')
     def markdown_filter(text):
         return markdown.markdown(text)
@@ -651,7 +651,7 @@ def create_app(config_class=Config):
 
                 session['admin_id'] = admin.admin_id
             else:
-                return "Invalid Secrect Key",403
+                return "Invalid Secrect Key", 403
         else:
             admin_id = os.environ.get('DEFAULT_ADMIN_ID')
             session['admin_id'] = admin_id
