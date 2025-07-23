a14579e43c52ceb616fc8e45721adc8022b6561f
humamchoudhary <humamchoudhary@gmail.com>
2025-07-23 15:29:51 +0500

    ui change

--- templates/js/init_chat.js
+++ templates/js/init_chat.js
@@ -374,9 +374,9 @@ function initializeChatbot() {
        hx-target="#chatbox"
        hx-swap="innerHTML"
        data-base-url="${config.backendUrl}">
-    <div style="display:flex; flex-direction:column;align-items:center; justify-content:center">
+    <div style="display:flex; flex-direction:column;align-items:center; justify-content:center; height:100%">
                     <svg class="spin " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
-style="animation: spin 1s linear infinite; color:white; width:45px; height:45px"
+style="animation: spin 1s linear infinite; color:white; width:25px; height:25px"
     >
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
