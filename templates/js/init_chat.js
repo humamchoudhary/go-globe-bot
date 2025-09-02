365c1d679bd1f77994c84af2851b19332c8e72dd
humamchoudhary <humamchoudhary@gmail.com>
2025-09-02 14:37:36 +0500

    update

--- templates/js/init_chat.js
+++ templates/js/init_chat.js
@@ -412,9 +412,13 @@ function initializeChatbot() {
         color: #ff5800;
         font-size: 24px;
     cursor: pointer;
-
     display:none
+transition:all 0.1s ease-in-out 0.1s;
       "
+
+ onMouseOver="this.style.opacity=0.7"
+   onMouseOut="this.style.opacity=1" 
+
     >
       <svg
         width="20"
@@ -437,7 +441,11 @@ function initializeChatbot() {
         color: #ff5800;
         font-size: 24px;
         cursor: pointer;
+transition:all 0.1s ease-in-out 0.1s;
       "
+
+ onMouseOver="this.style.opacity=0.7"
+   onMouseOut="this.style.opacity=1" 
     >
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M6.4 19L5 17.6L10.6 12L5 6.4L6.4 5L12 10.6L17.6 5L19 6.4L13.4 12L19 17.6L17.6 19L12 13.4L6.4 19Z" fill="#FF5800"/>
